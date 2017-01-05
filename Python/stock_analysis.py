1bf54620c541957d2debdfdab32022283a743f60
Gururaj Sridhar <gururajks1988@gmail.com>
2017-01-05 18:57:36 -0500

    Basic stock analysis
    Udacity courses.

---
+++ Python/stock_analysis.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_stock():
+    adsk_s = pd.read_csv('adsk_5y.csv')
+    #adsk_s.sort_values()
+    adsk_s[["High", "Low"]].plot()
+    plt.show()
+
+#create an empty data frame
+def create_empty_df(start_date, end_date):
+    dates = pd.date_range(start_date, end_date)
+    df = pd.DataFrame(index=dates)
+    return df
+
+#create a custom stock data frame from panda
+def create_stock_df(start_date, end_date, symbol):
+    df = create_empty_df(start_date, end_date)
+   
+    #join
+    df_stock = pd.read_csv("{}_5y.csv".format(symbol), 
+                        index_col="Date", 
+                        parse_dates=True, 
+                        usecols=["Date", "Adj Close"],
+                        na_values=['nan'])
+    df_stock = df_stock.rename(columns={'Adj Close': symbol})
+    df = df.join(df_stock, how='inner')
+    
+    return df
+
+#get the stock data for a list of symbols
+def get_stock_data(start_date, end_date, symbols):        
+    df = create_empty_df(start_date, end_date)
+    for symbol in symbols:
+        df_temp = create_stock_df(start_date, end_date, symbol)
+        df = df.join(df_temp)
+        df.dropna()
+    return df
+
+if __name__ == "__main__":
+    #run()
+    symbols = ['AMD', 'ADSK', 'NVDA', 'INTC']
+    start_date = "2012-01-01"
+    end_date = "2016-12-31"
+    
+    df = get_stock_data(start_date, end_date, symbols)
+    df_sliced = df.ix['2016-01-04':'2016-12-30']
+    df_sliced = df_sliced / df_sliced.ix[0,:]
+    df_sliced[['AMD', 'NVDA', 'INTC']].plot()
+    plt.show()
+    
+   
