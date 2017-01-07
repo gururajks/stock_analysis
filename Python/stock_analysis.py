721eba20f11c2ff74b08c7059e367d54a0c339ec
gururajks <gururaj.genius@gmail.com>
2017-01-06 19:22:48 -0500

    daily returns

--- Python/stock_analysis.py
+++ Python/stock_analysis.py
@@ -37,6 +37,36 @@ def get_stock_data(start_date, end_date, symbols):
         df = df.dropna()
     return df
 
+#plot normalize stocks
+def plot_normalize_stock(df_sliced, symbol):
+    #sliced normalized graph
+    df_sliced = df_sliced / df_sliced.ix[0,:]
+    df[symbol].plot()
+    plt.show()
+
+#plot the bollinger bands
+def plot_bollinger_bands(df_sliced, symbol):
+    df_sliced[symbol].plot(label="Actual")
+    
+    r_mean = pd.rolling_mean(df_sliced, window=20)
+    r_std = pd.rolling_std(df_sliced, window=20)
+    
+    r_mean[symbol].plot(label="MVA")
+    upper_band = r_mean + 2 * r_std
+    lower_band = r_mean + (-2) * r_std
+    
+    upper_band[symbol].plot(label="Upper Band")
+    lower_band[symbol].plot(label="Lower Band")
+    
+    plt.show()
+    
+#plot the daily returns
+def plot_daily_returns(df_sliced, symbol):
+    df_sliced = df_sliced[symbol][1:] / df_sliced[symbol][:-1].values
+    df_sliced.plot()
+    plt.show()
+    
+#main method
 if __name__ == "__main__":
     #run()
     symbols = ['AMD', 'ADSK', 'NVDA', 'INTC']
@@ -45,20 +75,6 @@ if __name__ == "__main__":
     
     df = get_stock_data(start_date, end_date, symbols)
     df_sliced = df.ix['2016-01-04':'2016-12-30']
-    #sliced normalized graph
-    #df_sliced = df_sliced / df_sliced.ix[0,:]
-    df_sliced[['AMD']].plot(label="Actual")
     
-    r_mean = pd.rolling_mean(df_sliced, window=20)
-    r_std = pd.rolling_std(df_sliced, window=20)
-    
-    r_mean['AMD'].plot(label="MVA")
-    upper_band = r_mean + 2 * r_std
-    lower_band = r_mean + (-2) * r_std
-    
-    upper_band['AMD'].plot(label="Upper Band")
-    lower_band['AMD'].plot(label="Lower Band")
-    
-    plt.show()
     
    
